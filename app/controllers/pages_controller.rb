f62fa144eb16f0d96cad335dcee64e0e2c437572
Erik Rumppe <erumppe@gmail.com>
2013-09-08 14:53:26 -0700

    Saving data to Google Drive spreadsheet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @title = 'Home'
-    @desc = 'This is a description that means nothing and should only be on the home page.'
     @owner = Owner.new
   end
   
   def about 
-    @title = 'About'
   end
   
-  def contact   
-    @title = 'Contact'
+  def contact
   end
 end
